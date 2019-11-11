ccedf9424e648c3fcb20bc844e8eaa6ac782c962
moraagarcia <57638789+moraagarcia@users.noreply.github.com>
2019-11-11 16:16:50 -0300

    Add files via upload

---
+++ src/main/java/com/codeoftheweb/salvo/models/Player.java
@@ -0,0 +1,43 @@
+package com.codeoftheweb.salvo.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+
+    private String userName;
+    private String password;
+
+    public Player() {
+    }
+
+    public Player(String userName) {
+        this.userName = userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
